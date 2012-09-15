0b8a1c3cbc108789f987e3860ab1e8cbd374d2df
Cesar Ramirez <cesar.ramsan@gmail.com>
2012-09-14 23:29:29 -0500

    Initial commit with code, empty android project created from eclipse

---
+++ src/com/cesarandres/campuscompass/MapActivity.java
@@ -0,0 +1,24 @@
+package com.cesarandres.campuscompass;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.support.v4.app.NavUtils;
+
+public class MapActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_map, menu);
+        return true;
+    }
+
+    
+}
